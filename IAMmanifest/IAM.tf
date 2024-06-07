ea52727189e1c38abdafea8b097f9ce5d854f92d
mahesh <maheshkuraganti9@gmail.com>
2024-06-07 16:23:46 +0530

    iam manifest create and commited

---
+++ IAMmanifest/IAM.tf
@@ -0,0 +1,10 @@
+#Provider Block
+provider "aws" {
+  region = "ap-south-1"
+  profile = "default"
+}
+
+resource "aws_iam_user" "buddipammukushi_user" {
+  name = "buddipammukushi-user"
+  path = "/"
+}
