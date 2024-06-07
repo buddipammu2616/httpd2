5c6b721b610d45affcfa070df0004d181e5e4dfa
mahesh <maheshkuraganti9@gmail.com>
2024-06-07 15:46:14 +0530

    again commited

--- s3bucketmanifest/main.tf
+++ s3bucketmanifest/main.tf
@@ -1,14 +1,21 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "ap-south-1"  # Update with your desired region
 }
 
-resource "aws_s3_bucket" "buddipammukushi" {
-  for_each = var.s3_buckets
-
-  bucket = buddipammukushi
-  #acl    = each.value.acl
-
-  tags = {
-    "Name" = each.value.buddipammukushi
+# Define a map variable containing the names of the buckets
+variable "bucket_names" {
+  type = map(string)
+  default = {
+    bucket1 = "maheshbuddipammukushimanaswi-1"
+    bucket2 = "maheshbuddipammukushimanaswi-2"
+    bucket3 = "maheshbuddipammukushimanaswi-3"
   }
+}
+
+# Create S3 buckets using for_each
+resource "aws_s3_bucket" "maheshbuddipammukushimanaswi" {
+  for_each = var.bucket_names
+
+  bucket = each.value
+  #acl    = "private"
 }
