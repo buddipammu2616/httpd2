f94a01b9e342604d0bbbabed513883ac47227149
mahesh <maheshkuraganti9@gmail.com>
2024-06-07 15:14:20 +0530

    commited

--- s3bucketmanifest/main.tf
+++ s3bucketmanifest/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_s3_bucket" "this" {
   for_each = var.s3_buckets
 
-  bucket = each.value.bucket_name
+  bucket = maheshbuddipammukushi
   #acl    = each.value.acl
 
   tags = {
