b921fcfab9f2b03009493bc856f01ebd04c8e450
mahesh <maheshkuraganti9@gmail.com>
2024-06-07 15:37:31 +0530

    commited

--- multiples3bucketsmanifest/s3.tf
+++ multiples3bucketsmanifest/s3.tf
@@ -1,8 +1,18 @@
-#Resource Block
-resource "aws_s3_bucket" "buddipammukushi" {
-    bucket = "buddipammukushi"
-    count = 3
-    tags = {
-      "Name" = "buddipammukushi-${count.index}"
-    }
+provider "aws" {
+  region = "ap-south-1"  # Update with your desired region
+}
+
+resource "aws_s3_bucket" "bucket1" {
+  bucket = "my-first-bucket"
+  #acl    = "private"
+}
+
+resource "aws_s3_bucket" "bucket2" {
+  bucket = "my-second-bucket"
+  #acl    = "private"
+}
+
+resource "aws_s3_bucket" "bucket3" {
+  bucket = "my-third-bucket"
+  #acl    = "private"
 }
