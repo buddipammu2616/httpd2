7a772a3c0ee509a14e807e3f15884cce2b4da2c0
mahesh <maheshkuraganti9@gmail.com>
2024-06-07 15:27:07 +0530

    again commited

--- multiples3bucketsmanifest/s3.tf
+++ multiples3bucketsmanifest/s3.tf
@@ -1,11 +1,7 @@
-#Provider block
-provider "aws" {
-  region = "ap-south-1"
-  profile = "default"
-}
-# List of bucket names to be created
-variable "bucket_names" {
-  description = "List of S3 bucket names"
-  type        = list(string)
-  default     = ["maheshbuddipammukushi-1", "maheshbuddipammukushi-2", "maheshbuddipammukushi-3"] 
+#Resource Block
+resource "aws_s3_bucket" "buddipammu" {
+    bucket = "buddipammu"
+    tags = {
+      "Name" = "buddipammu"
+    }
 }
