8d5a6eb6925e7d53d520aad2c4e595e897fb6d8d
mahesh <maheshkuraganti9@gmail.com>
2024-06-07 16:37:19 +0530

    Multiple iam users create and commited

--- IAMmanifest/IAM.tf
+++ IAMmanifest/IAM.tf
@@ -1,10 +1,31 @@
 #Provider Block
+#provider "aws" {
+  #region = "ap-south-1"
+  #profile = "default"
+#}
+
+#resource "aws_iam_user" "buddipammukushi_user" {
+  #name = "buddipammukushi_user"
+  #path = "/"
+#}
+
 provider "aws" {
   region = "ap-south-1"
-  profile = "default"
 }
 
-resource "aws_iam_user" "buddipammukushi_user" {
-  name = "buddipammukushi_user"
-  #path = "/"
+# Define a set of user names
+locals {
+  iam_users = [
+    "mahesh1",
+    "Ashvitha2",
+    "Yohith3",
+    "Manaswi4"
+  ]
+}
+
+# Create IAM users using for_each meta-argument
+resource "aws_iam_user" "user" {
+  for_each = toset(local.iam_users)
+
+  name = each.key
 }
