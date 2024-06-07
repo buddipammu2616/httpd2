2dc23fab95556de4299fe53e7ef79ac17040f89d
mahesh <maheshkuraganti9@gmail.com>
2024-06-07 15:41:24 +0530

    again comited

--- multiples3bucketsmanifest/s3.tf
+++ multiples3bucketsmanifest/s3.tf
@@ -2,17 +2,17 @@ provider "aws" {
   region = "ap-south-1"  # Update with your desired region
 }
 
-resource "aws_s3_bucket" "bucket1" {
-  bucket = "my-first-bucket"
+resource "aws_s3_bucket" "maheshbuddipamukushi-1" {
+  bucket = "maheshbuddipammukushi-1"
   #acl    = "private"
 }
 
-resource "aws_s3_bucket" "bucket2" {
-  bucket = "my-second-bucket"
+resource "aws_s3_bucket" "maheshbuddipammukushi-2" {
+  bucket = "maheshbuddipammukushi-2"
   #acl    = "private"
 }
 
-resource "aws_s3_bucket" "bucket3" {
-  bucket = "my-third-bucket"
+resource "aws_s3_bucket" "maheshbuddipammukush-3" {
+  bucket = "maheshbuddipammukush-3"
   #acl    = "private"
 }
