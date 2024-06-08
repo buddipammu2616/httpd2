fe62adbe0bc546addf9c478fbf5b4dde6dab8e22
mahesh <maheshkuraganti9@gmail.com>
2024-06-08 12:06:18 +0530

    ec2 manifest file create and commited

---
+++ ec2manifestfile/ec2latest.tf
@@ -0,0 +1,29 @@
+#Terraform block
+terraform {
+  required_version = "~>1.8.5"
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~>5.0"
+    }
+  }
+}
+#Provider Block
+provider "aws" {
+  region = "ap-south-1"
+  profile = "default"
+}
+
+#Resource Block
+resource "aws_instance" "maheshec2" {
+  availability_zone = "ap-south-1a"
+  ami = "ami-00fa32593b478ad6e"
+   key_name = "terraform-keypair"
+ instance_type = "t2.micro"
+ tags = {
+   "Name" = "maheshec2"
+ }
+ #lifecycle {
+  # create_before_destroy = "true"
+ #}
+}
